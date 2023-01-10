846214e4eba04efd170024dfc30de0e6122d4e4e
devalasa <ogboanohrichardjamb2020@gmail.com>
2023-01-10 23:01:43 +0100

    update on my assignment portal

--- assignment_portal/scripts/script.js
+++ assignment_portal/scripts/script.js
@@ -1,5 +1,32 @@
-let date = document.querySelector(".date")
-let currentTime = new Date();
-currentTime.setMinutes(currentTime.getMinutes() + 30);
-currentTime.setSeconds(currentTime.getSeconds() + 15);
-date.textContent = `Last updated: ${currentTime}`;
+// Long hand method ... building day and month names from built-in date methods.
+let daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Fridat",
+	"Saturday"
+];
+let months = [
+	"Jan",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = `Last Updated: ${dayName}, ${monthName} ${d.getDate()}, ${year}`;
+document.querySelector(".date").textContent = fulldate;
+let copyYear = `${year} .:|:. Richard O. Ogboanoh .:|:. Nigeria`
+document.querySelector(".copy").textContent = copyYear;
