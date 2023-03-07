3c8bf123a35f890b2c9ea77fa0e10e40d6c26240
devalasa <ogboanohrichardjamb2020@gmail.com>
2023-03-07 18:42:38 +0100

    incoming...

--- lesson7/scripts/script.js
+++ lesson7/scripts/script.js
@@ -1,54 +1,27 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
+const loadImage = image => {
+    const src = image.getAttribute("data-src");
+    image.setAttribute("src", src);
     image.onload = () => {
-        image.removeAttribute("data-src")
-    };
+        image.removeAttribute("data-src");
+    }
 };
-
 const imgOptions = {
+    
 }
 
-if ("IntersectionObserver" in window) {
+if("IntersectionObserver" in window) {
+    console.log("Observer works")
     const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
+        items.forEach(item => {
+            if(item.isIntersecting) {
+                loadImage(item.target);
                 observer.unobserve(item.target);
             }
-        }, imgOptions);
-    })
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    })
-}   else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img)
-    })
+        })
+    }, imgOptions);
+    imagesToLoad.forEach(img => observer.observe(img))
+}else {
+    imagesToLoad.forEach(img => {loadImage(img)});
 }
-
-
-
-
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-})
-
-// if ("IntersectionObserver" in window) {
-//     const observer = new IntersectionObserver((items, observer) => {
-//         items.forEach((item) => {
-//             if (item.isIntersecting) {
-//                 loadImages(item.target);
-//                 observer.unobserve(item.target);
-//             }
-//         });
-//     })
-//     imagesToLoad.forEach((img) => {
-//         observer.observe(img);
-//     })
-// }   else {
-//     imagesToLoad.forEach((img) => {
-//         loadImages(img)
-//     })
-// }
